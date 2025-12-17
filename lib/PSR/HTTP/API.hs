738a94f4206341279b40c1b616ea2b6268d458d9
Alex Mason <alex.mason@tweag.io>
2025-12-17 15:23:42 +1100

    Convert API to use NamedRoutes

--- lib/PSR/HTTP/API.hs
+++ lib/PSR/HTTP/API.hs
@@ -1,17 +1,20 @@
+{- HLINT ignore "Use newtype instead of data" -}
 module PSR.HTTP.API (
     ServerAPI,
-    EventType(..),
+    EventType (..),
     FilterQueryParams (..),
+    SiteRoutes (..),
+    EventRoutes (..),
+    siteApi,
 ) where
 
 import Data.Aeson (FromJSON, ToJSON)
 import Data.Text (Text)
 import Data.Time.Clock (UTCTime)
-import GHC.Generics
+import GHC.Generics (Generic)
 import Servant
 import Servant.QueryParam.Record (RecordParam)
-import Servant.QueryParam.TypeLevel
-import Servant.Server.Generic ()
+import Servant.QueryParam.TypeLevel (DropPrefix, Eval, Exp)
 
 data Event = Event
     { content :: Text
@@ -38,20 +41,31 @@ instance FromHttpApiData EventType where
         _ -> Left "Unknown event type"
 
 data FilterQueryParams = FilterQueryParams
-    { _filterQueryParam_type :: (Maybe EventType)
-    , _filterQueryParam_time_begin :: (Maybe UTCTime)
-    , _filterQueryParam_time_end :: (Maybe UTCTime)
-    , _filterQueryParam_slot_begin :: (Maybe Integer)
-    , _filterQueryParam_slot_end :: (Maybe Integer)
-    , _filterQueryParam_limit :: (Maybe Integer)
-    , _filterQueryParam_offset :: (Maybe Integer)
-    , _filterQueryParam_name :: (Maybe Text)
+    { _filterQueryParam_type :: Maybe EventType
+    , _filterQueryParam_time_begin :: Maybe UTCTime
+    , _filterQueryParam_time_end :: Maybe UTCTime
+    , _filterQueryParam_slot_begin :: Maybe Integer
+    , _filterQueryParam_slot_end :: Maybe Integer
+    , _filterQueryParam_limit :: Maybe Integer
+    , _filterQueryParam_offset :: Maybe Integer
+    , _filterQueryParam_name :: Maybe Text
     }
     deriving (Generic)
 
-type ServerAPI =
-    "events"
-        :> RecordParam DropPrefixExp FilterQueryParams
-        :> ( Get '[JSON] [Event]
-                :<|> Capture "script_hash_or_name" Text :> Get '[JSON] [Event]
-           )
+type EventFilterParams = RecordParam DropPrefixExp FilterQueryParams
+
+data EventRoutes route = EventRoutes
+    { allEvents :: route :- EventFilterParams :> Get '[JSON] [Event]
+    , namedEvents :: route :- EventFilterParams :> Capture "script_hash_or_name" Text :> Get '[JSON] [Event]
+    }
+    deriving (Generic)
+
+data SiteRoutes route = SiteRoutes
+    { events :: route :- "events" :> NamedRoutes EventRoutes
+    }
+    deriving (Generic)
+
+type ServerAPI = NamedRoutes SiteRoutes
+
+siteApi :: Proxy ServerAPI
+siteApi = Proxy
--- lib/PSR/HTTP/Server.hs
+++ lib/PSR/HTTP/Server.hs
@@ -3,8 +3,9 @@ module PSR.HTTP.Server (
 ) where
 
 import PSR.HTTP.API
-import PSR.Storage.Interface (Storage(..))
+import PSR.Storage.Interface (Storage (..))
 
+import Control.Applicative ((<|>))
 import Control.Monad.IO.Class (liftIO)
 import Data.Default (def)
 import Network.Wai.Handler.Warp qualified as Warp
@@ -13,24 +14,32 @@ import Prometheus (register)
 import Prometheus.Metric.GHC (ghcMetrics)
 import Servant
 import Servant.QueryParam.Server.Record ()
-import Servant.Server.Generic ()
+import Servant.Server.Generic (AsServer)
 
-serverApi :: Proxy ServerAPI
-serverApi = Proxy
-
-server :: Storage -> Server ServerAPI 
-server Storage{..} = eventsH
+server :: Storage -> Server ServerAPI
+server Storage{..} = siteH
   where
-    eventsH params = (eventsHandler params Nothing) :<|> (eventsHandler params . Just)
+    siteH :: SiteRoutes AsServer
+    siteH =
+        SiteRoutes
+            { events = eventsH
+            }
+
+    eventsH :: EventRoutes AsServer
+    eventsH =
+        EventRoutes
+            { allEvents = \params -> eventsHandler params Nothing
+            , namedEvents = \params -> eventsHandler params . Just
+            }
 
     eventsHandler filterParams' mName = do
-      -- The capture parameter `name` has a higher priority over the query param
-      let nameFilterParameter = maybe (_filterQueryParam_name filterParams') Just mName
-      let filterParams = filterParams' { _filterQueryParam_name = nameFilterParameter }
-      _events <- liftIO $ getEvents filterParams
-      pure []
+        -- The capture parameter `name` has a higher priority over the query param
+        let nameFilterParameter = mName <|> _filterQueryParam_name filterParams'
+        let filterParams = filterParams'{_filterQueryParam_name = nameFilterParameter}
+        _events <- liftIO $ getEvents filterParams
+        pure []
 
 run :: Storage -> Warp.Port -> IO ()
 run storage port = do
-  _ <- register ghcMetrics
-  Warp.run port (prometheus def $ serve serverApi (server storage))
+    _ <- register ghcMetrics
+    Warp.run port (prometheus def $ serve siteApi (server storage))

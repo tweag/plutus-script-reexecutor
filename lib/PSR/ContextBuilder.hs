e4166ccfc60f6869a3a98fe6873423b60737557b
Adithya Kumar <adithya.kumar@tweag.io>
2025-12-18 15:48:42 +0530

    Update the BlockContext and add an API to evaluate scripts

--- lib/PSR/ContextBuilder.hs
+++ lib/PSR/ContextBuilder.hs
@@ -5,6 +5,7 @@ module PSR.ContextBuilder (
     TransactionContext (..),
     mkBlockContext,
     mkTransactionContext,
+    evaluateTransaction,
 ) where
 
 --------------------------------------------------------------------------------
@@ -13,21 +14,16 @@ module PSR.ContextBuilder (
 
 import Cardano.Api qualified as C
 import Cardano.Api.Ledger qualified as L
-import Cardano.Ledger.Api.Scripts qualified as S
-import Cardano.Ledger.Plutus qualified as L
 import Control.Monad (guard)
-import Control.Monad.Trans.Writer (WriterT (runWriterT))
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Maybe (mapMaybe)
 import Data.Set (Set)
 import Data.Set qualified as Set
+import Data.Text (Text)
 import PSR.Chain
-import PSR.ConfigMap (ConfigMap (..), ResolvedScript (..), ScriptEvaluationParameters (..))
-import PlutusLedgerApi.Common
-import PlutusLedgerApi.V1.EvaluationContext qualified as V1
-import PlutusLedgerApi.V2.EvaluationContext qualified as V2
-import PlutusLedgerApi.V3.EvaluationContext qualified as V3
+import PSR.ConfigMap (ConfigMap (..), ResolvedScript (..))
+import PSR.Evaluation.Api (evaluateTransactionExecutionUnitsShelley)
 
 --------------------------------------------------------------------------------
 -- Block Context
@@ -43,16 +39,19 @@ import PlutusLedgerApi.V3.EvaluationContext qualified as V3
 data BlockContext era where
     BlockContext ::
         { ctxPrevChainPoint :: C.ChainPoint
-        , ctxShelleyBasedEra :: C.ShelleyBasedEra era
+        , -- TODO: Use AlonzoEraOnwards here instead of ShelleyBasedEra
+          ctxShelleyBasedEra :: C.ShelleyBasedEra era
         , ctxTransactions :: [C.Tx era]
-        , ctxInputUtxoMap :: Map C.TxIn (C.TxOut C.CtxUTxO era)
+        , ctxInputUtxoMap :: C.UTxO era
         , -- NOTE: The protocol parameters (and hence the cost models) may change
           -- in a running node. It may be okay to poll this at the block boundary.
-          ctxCostModels :: S.CostModels
+          ctxPParams :: L.PParams (C.ShelleyLedgerEra era)
+        , ctxEraHistory :: C.EraHistory
+        , ctxSysStart :: C.SystemStart
         } ->
         BlockContext era
 
-deriving instance Show (BlockContext era)
+-- deriving instance Show (BlockContext era)
 
 mkBlockContext ::
     C.LocalNodeConnectInfo ->
@@ -62,19 +61,13 @@ mkBlockContext ::
     IO (BlockContext era)
 mkBlockContext conn prevCp era txs = do
     let query =
-            (,)
+            BlockContext prevCp era txs
                 <$> utxoMapQuery era txs
-                <*> costModelsQuery era
+                <*> pParamsQuery era
+                <*> eraHistoryQuery
+                <*> sysStartQuery
     -- NOTE: We can catch CostModelsQueryException and choose to retry or skip.
-    (umap, costs) <- runLocalStateQueryExpr conn prevCp query
-    pure $
-        BlockContext
-            { ctxPrevChainPoint = prevCp
-            , ctxShelleyBasedEra = era
-            , ctxTransactions = txs
-            , ctxInputUtxoMap = C.unUTxO umap
-            , ctxCostModels = costs
-            }
+    runLocalStateQueryExpr conn prevCp query
 
 --------------------------------------------------------------------------------
 -- Transaction Context
@@ -84,7 +77,6 @@ data TransactionContext era where
     TransactionContext ::
         { ctxTransaction :: C.Tx era
         , ctxRelevantScripts :: Map.Map C.ScriptHash ResolvedScript
-        , ctxEvaluationContext :: Map C.ScriptHash EvaluationContext
         } ->
         TransactionContext era
 
@@ -109,46 +101,31 @@ getNonEmptyIntersection ::
     C.Tx era ->
     Maybe (Map.Map C.ScriptHash ResolvedScript)
 getNonEmptyIntersection ConfigMap{..} BlockContext{..} tx = do
-    let interestingScripts =
+    let inpUtxoMap = C.unUTxO ctxInputUtxoMap
+        interestingScripts =
             Map.restrictKeys cmScripts $
-                Set.union (getMintPolicies tx) (getInputScriptAddrs ctxInputUtxoMap tx)
+                Set.union (getMintPolicies tx) (getInputScriptAddrs inpUtxoMap tx)
     guard (not $ Map.null interestingScripts)
-    pure $ interestingScripts
-
-makeEvaluationContext ::
-    S.CostModels ->
-    PlutusLedgerLanguage ->
-    C.ExceptT String IO EvaluationContext
-makeEvaluationContext params lang = case lang of
-    PlutusV1 -> run L.PlutusV1 V1.mkEvaluationContext
-    PlutusV2 -> run L.PlutusV2 V2.mkEvaluationContext
-    PlutusV3 -> run L.PlutusV3 V3.mkEvaluationContext
-  where
-    run lng f = case Map.lookup lng (L.costModelsValid params) of
-        Just costs -> C.modifyError show . fmap fst . runWriterT $ f (L.getCostModelParams costs)
-        Nothing -> C.throwError $ "Unknown cost model for lang: " ++ show lang
-
-getScriptEvaluationContext ::
-    BlockContext era ->
-    Map C.ScriptHash ResolvedScript ->
-    IO (Maybe (Map C.ScriptHash EvaluationContext))
-getScriptEvaluationContext BlockContext{..} relevantScripts = do
-    let langs :: Map C.ScriptHash PlutusLedgerLanguage
-        langs = Map.mapMaybe (fmap (sepLanguage . fst) . rsScriptForEvaluation) relevantScripts
-    res <- C.runExceptT $ traverse (makeEvaluationContext ctxCostModels) langs
-    case res of
-        Left err -> Nothing <$ putStrLn err
-        Right evalContexts -> do
-            C.liftIO $ print (Map.keys evalContexts)
-            pure $ Just $ evalContexts
+    pure interestingScripts
 
 mkTransactionContext ::
     ConfigMap -> BlockContext era -> C.Tx era -> IO (Maybe (TransactionContext era))
 mkTransactionContext cm bc tx = do
     case getNonEmptyIntersection cm bc tx of
         Nothing -> pure Nothing
-        Just nei -> do
-            msec <- getScriptEvaluationContext bc nei
-            pure $ case msec of
-                Nothing -> Nothing
-                Just sec -> Just $ TransactionContext tx nei sec
+        Just nei -> pure $ Just $ TransactionContext tx nei
+
+evaluateTransaction ::
+    BlockContext era ->
+    C.Tx era ->
+    Map
+        C.ScriptWitnessIndex
+        (Either C.ScriptExecutionError ([Text], C.ExecutionUnits))
+evaluateTransaction BlockContext{..} (C.ShelleyTx era tx) = do
+    evaluateTransactionExecutionUnitsShelley
+        era
+        ctxSysStart
+        (C.toLedgerEpochInfo ctxEraHistory)
+        (C.LedgerProtocolParameters ctxPParams)
+        ctxInputUtxoMap
+        tx

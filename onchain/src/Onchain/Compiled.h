757e7fc8a301fad288fd66c76ab30bb42bd2aea1
Adithya Kumar <adithya.kumar@tweag.io>
2026-01-01 21:50:24 +0530

    Use plutus v2 in the simple script and trace the trigger purpose

--- onchain/src/Onchain/Compiled.h
+++ onchain/src/Onchain/Compiled.h
@@ -14,17 +14,24 @@ import Onchain.Escrow qualified as Escrow
 import PlutusTx
 import PlutusTx.Prelude
 import Cardano.Api
+import PlutusCore.Version (plcVersion100)
 
 --------------------------------------------------------------------------------
 -- Compiled
 --------------------------------------------------------------------------------
 
-alwaysTrue :: CompiledCodeLang PlutusScriptV3 (BuiltinData -> BuiltinUnit)
-alwaysTrue = CompiledCodeLang $$(PlutusTx.compile [|| Simple.alwaysTrue ||])
+type PolicyV2 = BuiltinData -> BuiltinData -> BuiltinUnit
+type ValidatorV2 = BuiltinData -> BuiltinData -> BuiltinData -> BuiltinUnit
+type CompiledCodePolicyV2 = CompiledCodeLang PlutusScriptV2 PolicyV2
+type CompiledCodeValidatorV2 = CompiledCodeLang PlutusScriptV2 ValidatorV2
 
-escrowValidator ::
-    Escrow.EscrowParams ->
-    CompiledCodeLang PlutusScriptV2 (BuiltinData -> BuiltinData -> BuiltinData -> BuiltinUnit)
+tracingPolicy :: CompiledCodePolicyV2
+tracingPolicy = CompiledCodeLang $$(PlutusTx.compile [|| Simple.policy ||])
+
+tracingValidator :: CompiledCodeValidatorV2
+tracingValidator = CompiledCodeLang $$(PlutusTx.compile [|| Simple.validator ||])
+
+escrowValidator :: Escrow.EscrowParams -> CompiledCodeValidatorV2
 escrowValidator params =
     CompiledCodeLang
-        ($$(PlutusTx.compile [|| Escrow.validatorUntyped ||]) `unsafeApplyCode` liftCodeDef params)
+        ($$(PlutusTx.compile [|| Escrow.validatorUntyped ||]) `unsafeApplyCode` liftCode plcVersion100 params)

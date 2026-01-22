90625b16a1579fa5d9701270d05fae35c5e4b97f
Adithya Kumar <adithya.kumar@tweag.io>
2026-01-22 16:02:09 +0530

    Trace the entire script context in V3 tests

--- onchain/src/Onchain/V3/Simple.hs
+++ onchain/src/Onchain/V3/Simple.hs
@@ -4,6 +4,7 @@ module Onchain.V3.Simple where
 -- Imports
 --------------------------------------------------------------------------------
 
+import Onchain.V3.ShowBS
 import PlutusLedgerApi.V3
 import PlutusTx.Prelude
 
@@ -11,17 +12,10 @@ import PlutusTx.Prelude
 -- Scripts
 --------------------------------------------------------------------------------
 
-{-# INLINEABLE traceScriptPurpose #-}
-traceScriptPurpose :: ScriptContext -> Bool
-traceScriptPurpose sCtx =
-    case scriptContextScriptInfo sCtx of
-        MintingScript _ -> trace "MintingScript" True
-        SpendingScript _ _ -> trace "SpendingScript" True
-        RewardingScript _ -> trace "RewardingScript" True
-        CertifyingScript _ _ -> trace "CertifyingScript" True
-        VotingScript _ -> trace "VotingScript" True
-        ProposingScript _ _ -> trace "ProposingScript" True
+{-# INLINEABLE traceFullContext #-}
+traceFullContext :: ScriptContext -> Bool
+traceFullContext sCtx = trace (showBS sCtx) True
 
 {-# INLINEABLE script #-}
 script :: BuiltinData -> BuiltinUnit
-script sCtx = check $ traceScriptPurpose (unsafeFromBuiltinData sCtx)
+script sCtx = check $ traceFullContext (unsafeFromBuiltinData sCtx)

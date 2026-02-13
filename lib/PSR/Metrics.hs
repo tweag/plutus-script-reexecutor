306c803e30519e1092b3362a6cf96a4adcdac8c9
Adithya Kumar <adithya.kumar@tweag.io>
2026-02-13 14:07:53 +0530

    Observe the growth of the auxillary state

--- lib/PSR/Metrics.hs
+++ lib/PSR/Metrics.hs
@@ -2,12 +2,17 @@ module PSR.Metrics (
     regSummary,
     standardQuantiles,
     regCounter,
+    regGauge,
     -- Re-exports
     Summary,
     observeDuration,
+    observe,
     Counter,
     incCounter,
     incCounterBy,
+    Gauge,
+    getGauge,
+    setGauge,
 ) where
 
 import Control.Monad.IO.Class (MonadIO)
@@ -20,6 +25,9 @@ standardQuantiles = defaultQuantiles ++ [(0.999, 0.001)]
 
 regSummary :: (MonadIO m) => Text -> Text -> m Summary
 regSummary name info = register $ summary (Info name info) standardQuantiles
+
+regGauge :: (MonadIO m) => Text -> Text -> m Gauge
+regGauge name info = register $ gauge (Info name info)
 
 regCounter :: Text -> Text -> IO Counter
 regCounter name info = register $ counter (Info name info)
--- lib/PSR/Streaming.hs
+++ lib/PSR/Streaming.hs
@@ -35,7 +35,7 @@ import Ouroboros.Network.Protocol.ChainSync.Client (
 import PSR.Chain
 import PSR.ConfigMap qualified as CM
 import PSR.ContextBuilder
-import PSR.Metrics (Counter, Summary, incCounter, incCounterBy, observeDuration, regCounter, regSummary)
+import PSR.Metrics (Counter, Gauge, Summary, getGauge, incCounter, incCounterBy, observeDuration, regCounter, regGauge, regSummary, setGauge)
 import PSR.Types
 import PlutusLedgerApi.Common (
     MajorProtocolVersion (MajorProtocolVersion),
@@ -286,6 +286,12 @@ mainLoop events cm@CM.ConfigMap{..} points = do
                 prevUtxoMap <- getUtxoMap
                 let (newUtxoMap, selectedTxs) =
                         selectScriptTriggeredTxs confHashes prevUtxoMap txList
+                -- TODO: It is possible to maintain this count at all times. We
+                -- should do that instead of going through our map each and
+                -- every time.
+                observeMaxUtxoMapValue
+                    metrics.max_internal_utxo_map_size
+                    (Map.size newUtxoMap)
                 -- NOTE: In most cases the list of selected transactions is
                 -- going to be empty. It is non-empty if and only if,
                 -- 1. The block has transactions that involve script executions
@@ -298,6 +304,12 @@ mainLoop events cm@CM.ConfigMap{..} points = do
                 Nothing -> pure mUtxoMap
                 Just era -> withAlonzoEra era
 
+observeMaxUtxoMapValue :: Gauge -> Int -> IO ()
+observeMaxUtxoMapValue gauge currVal0 = do
+    let currVal = fromIntegral currVal0
+    prevVal <- getGauge gauge
+    when (currVal > prevVal) $ setGauge gauge currVal
+
 --------------------------------------------------------------------------------
 -- Module metrics
 --------------------------------------------------------------------------------
@@ -306,6 +318,8 @@ data StreamingMetrics = StreamingMetrics
     { mainLoop_consumeBlock_runtime :: Summary
     , blocks_since_start :: Counter
     , tx_since_start :: Counter
+    , -- TODO: Make the name consice.
+      max_internal_utxo_map_size :: Gauge
     }
 
 initialiseMetrics :: IO StreamingMetrics
@@ -322,4 +336,5 @@ initialiseMetrics = do
         regCounter
             "tx_since_start"
             "The number of transactions seen since application start"
+    max_internal_utxo_map_size <- regGauge "max_internal_utxo_map_size" ""
     pure StreamingMetrics{..}
